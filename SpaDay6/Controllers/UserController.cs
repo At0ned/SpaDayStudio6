9b80d86868c5413ef725ead6a9d75b47cacaf0a1
At0ned <sethaholt@gmail.com>
2024-06-04 15:16:03 -0500

    Studio 2 done

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay6;
+[Route("/user")]
+public class UserController : Controller
+{
+
+[HttpGet]
+[Route("add")]
+public IActionResult AddUser()
+{
+return View("Add");
+}
+
+[HttpPost("/user")]
+   public IActionResult SubmitAddUserForm(User newUser, string verify)
+   {
+
+    if(newUser.Password != verify)
+    {
+    ViewBag.Error = "Passwords don't match.";
+    ViewBag.User = newUser;
+    return View("Add");
+    }
+    ViewBag.Username = newUser.Username;
+    ViewBag.Email = newUser.Email;
+    return View("Index");
+    
+   }
+}
